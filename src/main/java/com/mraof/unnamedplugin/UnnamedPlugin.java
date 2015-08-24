f2a23126cf7cf711ce4d97274266081716734149
mraof <mraoflink@verizon.net>
2015-08-24 16:00:18 -0400

    Initial commit

---
+++ src/main/java/com/mraof/unnamedplugin/UnnamedPlugin.java
@@ -0,0 +1,7 @@
+package com.mraof.unnamedplugin;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "UnnamedPlugin", name = "UnnamedPlugin", version = "0")
+public class UnnamedPlugin
+{
+}
